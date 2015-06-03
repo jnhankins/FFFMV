434154946ea53a8d93da31bab1162fcf1e8e9dff
unknown <j.n.hankins@gmail.com>
2015-06-02 19:46:15 -0500

    Updated headers

--- FFFMV/src/fff/mv/test/Test.java
+++ FFFMV/src/fff/mv/test/Test.java
@@ -1,21 +1,19 @@
 /**
- * FastFlameFractalMusicVideo (FFF)
- * An application for creating music videos using flame fractals.
- * Copyright (C) 2015  Jeremiah N. Hankins
- * 
- * This program is free software; you can redistribute it and/or modify it under
+ * FFFMV - An application for creating music videos using flame fractals.
+ * Copyright (C) 2015 Jeremiah N. Hankins
+ *
+ * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 3 of the License, or (at your option) any later
+ * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
  *
  * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 51
- * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package fff.mv.test;
