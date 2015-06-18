60103204de446574a504320b76ef76adf1f57d5c
unknown <j.n.hankins@gmail.com>
2015-06-17 23:19:55 -0500

    Created IndexedSkipList and SimpleMovingMedian to help with audio analysis for audio effects.

--- FFFMV/src/fff/mv/core/audio/AudioDataTask.java
+++ FFFMV/src/fff/mv/core/audio/AudioDataTask.java
@@ -1,18 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * FFFMV - An application for creating music videos using flame fractals.
+ * Copyright (C) 2015 Jeremiah N. Hankins
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package fff.mv.core.audio;
 
 import fff.mv.core.io.XugglerAudioInputStream;
-import fff.mv.core.io.XugglerErrorException;
 import java.io.File;
-import java.io.IOException;
 import java.lang.Thread.UncaughtExceptionHandler;
 import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
@@ -20,7 +30,9 @@ import javax.sound.sampled.AudioInputStream;
 import org.jtransforms.fft.FloatFFT_1D;
 
 /**
- *
+ * {@code AudioDataTask} contains a worker thread that generates
+ * {@link AudioData}.
+ * 
  * @author Jeremiah N. Hankins
  */
 public class AudioDataTask {
